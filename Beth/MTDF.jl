4ebe185c870a64255c584107c43fa2ece68f31a0
Markus Böck <markus.boeck7800@hotmail.com>
2020-12-29 23:38:04 +0100

    memory and mtdf works?

--- Beth/MTDF.jl
+++ Beth/MTDF.jl
@@ -1,8 +1,19 @@
 # https://people.csail.mit.edu/plaat/mtdf.html#abmem
 
 #                                       depth lower upper
-TranspositionTable = Dict{Board, Tuple{Int,Float64,Float64}}
-AlphaBetaMemory = Dict{Node, Tuple{Int,Float64,Float64}}
+TranspositionTable = Dict{String, Tuple{Int,Float64,Float64}}
+AlphaBetaMemory = Dict{String, Tuple{Int,Float64,Float64}}
+
+function key(node::Node)::String
+    key = ""
+    if node.move != (0x00, 0x00, 0x00)
+        while node.parent != nothing
+            key = string(node.move) * " " * key
+            node = node.parent
+        end
+    end
+    return key
+end
 
 function AlphaBetaWithMemory(beth::Beth, node::Node, depth::Int, α::Float64, β::Float64, white::Bool, tt::AlphaBetaMemory)
     beth.n_explored_nodes += 1
@@ -10,8 +21,8 @@ function AlphaBetaWithMemory(beth::Beth, node::Node, depth::Int, α::Float64, β
     @assert _white == white
 
     # transposition table look up
-    if haskey(tt, node)
-        d, lower, upper = tt[node]
+    if haskey(tt, key(node))
+        d, lower, upper = tt[key(node)]
         if lower ≥ β
             return lower
         end
@@ -60,22 +71,17 @@ function AlphaBetaWithMemory(beth::Beth, node::Node, depth::Int, α::Float64, β
         end
     end
 
-    # if depth > 0
-    #     _white, = restore_board_position(beth, node)
-    # end
-    @assert _α ≤ _β
-
     if value ≤ _α
         # Fail low result implies an upper bound
-        tt[node] = (depth, -Inf, value)
+        tt[key(node)] = (depth, -Inf, value)
     end
     if α < value && value < β
-        # Found an accurate minimax value - will not occur if called with zero window (α = 1 - β)
-        tt[node] = (depth, value, value)
+        # Found an accurate minimax value - will not occur if called with zero window (α = β-1)
+        tt[key(node)] = (depth, value, value)
     end
     if β ≤ value
         # Fail high result implies a lower bound
-        tt[node] = (depth, value, Inf)
+        tt[key(node)] = (depth, value, Inf)
     end
 
     return value
@@ -97,8 +103,32 @@ function alphabeta_search(beth::Beth; board=beth.board, white=beth.white, verbos
 end
 
 
-function MTDF(root::Node, guess::Float64, depth::Integer)
+function MTDF(beth::Beth; board=beth.board, white=beth.white, guess::Float64, depth::Integer)
+    value = guess
+    upper = Inf
+    lower = -Inf
 
+    mem = AlphaBetaMemory()
+    _,t = @timed while true
+        β = value == lower ? value + 1 : value
+
+        value = AlphaBetaWithMemory(beth, Node(), depth, β-1, β, white, mem)
+        @info("value: $value, beta: $β")
+        if value < β
+            upper = value
+        else
+            lower = value
+        end
+
+        if lower ≥ upper
+            break
+        end
+    end
+
+    @info(@sprintf "%d nodes (%d leafes) explored in %.4f seconds (%.2f/s)." beth.n_explored_nodes beth.n_leafes t (beth.n_explored_nodes/t) )
+
+
+    return value
 end
 
 function IMTDF(;max_depth::Integer, max_time::Float64)
@@ -118,5 +148,6 @@ print_tree(root, max_depth=1)
 
 root, mem = alphabeta_search(beth, board=deepcopy(pz.board), white=pz.white_to_move)
 
-
 root, mem = alphabeta_search(beth, board=deepcopy(pz.board), white=pz.white_to_move, mem=mem)
+
+MTDF(beth,  board=deepcopy(pz.board), white=pz.white_to_move, guess=0., depth=4)
