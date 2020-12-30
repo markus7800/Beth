8476dbb9a44e07c0c65f50773fd2c4dd78b7f1aa
Markus Böck <markus.boeck7800@hotmail.com>
2020-12-31 00:35:44 +0100

    puzzle rush

---
+++ puzzles/puzzle_rush_20_12_30.jl
@@ -0,0 +1,213 @@
+rush_20_12_30 = Puzzle[]
+
+puzzle = PuzzleFEN(
+    FEN="r1bqkbnr/ppp3pp/3p1p2/4p3/1n3P1P/3BPN2/PPPP2P1/RNBQK2R w KQkq - 0 1",
+    solution="Nd3",
+    firstmove="c3",
+    difficulty=194)
+push!(rush_20_12_30, puzzle)
+
+puzzle = PuzzleFEN(
+    FEN="8/8/8/8/1k6/3K4/2R1r3/8 b - - 16 63",
+    solution="Re2",
+    firstmove="Kb3",
+    difficulty=160)
+push!(rush_20_12_30, puzzle)
+
+puzzle = PuzzleFEN(
+    FEN="2kr1b1r/ppp1p2p/2n2n2/3qp1N1/2NP2p1/2P5/PP2Q1PP/R1B2RK1 w - - 0 1",
+    solution="ef6",
+    firstmove="Rf6",
+    difficulty=254)
+push!(rush_20_12_30, puzzle)
+
+puzzle = PuzzleFEN(
+    FEN="r2qkbnr/ppp2ppp/2n1p1b1/3p4/4PP2/3P1N2/PPPN2PP/R1BQKB1R w KQkq - 0 1",
+    solution="Qh4",
+    firstmove="Nh4",
+    difficulty=310)
+push!(rush_20_12_30, puzzle)
+
+puzzle = PuzzleFEN(
+    FEN="r3k2r/2P2p2/7P/6P1/1p6/3bBn1P/P4K2/2R5 b - - 0 1",
+    solution="c8Q Rc8 Rc8 Ke7 Rh8",
+    firstmove="Bb5", # ??
+    difficulty=362)
+push!(rush_20_12_30, puzzle)
+
+puzzle = PuzzleFEN(
+    FEN="rnb1kbnr/ppp3pp/4p3/4Pp2/3P3q/8/PPP3PP/RNBQKBNR w KQkq - 0 1",
+    solution="Qe4 Qe2 Qh8",
+    firstmove="g3",
+    difficulty=421)
+push!(rush_20_12_30, puzzle)
+
+puzzle = PuzzleFEN(
+    FEN="6k1/3R3p/6p1/1r2KpP1/4n2P/5N2/8/8 w - - 0 1",
+    solution="Nc5 Kd6 Nd7",
+    firstmove="Ke6",
+    difficulty=473)
+push!(rush_20_12_30, puzzle)
+
+#???
+# puzzle = PuzzleFEN(
+#     FEN="2r2rk1/p4p1p/6p1/3p4/1n3q2/PPQ2N2/5PPP/2R2RK1 w - - 0 1",
+#     solution="Rc1 Rc1 Qc1 Ne1",
+#     firstmove="Qb4",
+#     difficulty=522)
+# push!(rush_20_12_30, puzzle)
+
+puzzle = PuzzleFEN(
+    FEN="rnbqkbnr/pp2pppp/2pp4/6B1/3P4/8/PPP1PPPP/RN1QKBNR w KQkq - 0 1",
+    solution="Qa5 Nc3 Qg5",
+    firstmove="e3",
+    difficulty=588)
+push!(rush_20_12_30, puzzle)
+
+
+puzzle = PuzzleFEN(
+    FEN="4k3/2r3R1/p3pP2/4P3/2pK3p/P2p3P/1P6/8 w - - 0 1",
+    solution="d2 f7 Kf8 Kc3 d1Q", # ??
+    firstmove="Rc7",
+    difficulty=633)
+push!(rush_20_12_30, puzzle)
+
+puzzle = PuzzleFEN(
+    FEN="8/6R1/5p1p/4n2k/4N3/4p1P1/2r5/7K b - - 0 54",
+    solution="Nf6",
+    firstmove="e2",
+    difficulty=674)
+push!(rush_20_12_30, puzzle)
+
+puzzle = PuzzleFEN(
+    FEN="r4r1k/1pp1bppp/p2p4/2n2P2/4P3/2P1B2P/qPQN2P1/1R3RK1 b - - 0 1",
+    solution="Rba1 Qa1 Ra1",
+    firstmove="Nd7",
+    difficulty=713)
+push!(rush_20_12_30, puzzle)
+
+puzzle = PuzzleFEN(
+    FEN="6k1/pp3pp1/2p1b2p/4P3/2P5/qP1Q2PP/2B4K/8 b - - 0 33",
+    solution="Qd8 Qf8 Bh7 Kh7 Qf8",
+    firstmove="Qc5",
+    difficulty=789)
+push!(rush_20_12_30, puzzle)
+
+puzzle = PuzzleFEN(
+    FEN="8/8/R6K/1r4P1/pk6/8/8/8 b - - 1 62",
+    solution="Ra5 Ka5 g6 Kb4 g7",
+    firstmove="Ra5",
+    difficulty=846)
+push!(rush_20_12_30, puzzle)
+
+puzzle = PuzzleFEN(
+    FEN="3r1rk1/2p1bppp/p7/1p2P3/5P2/1KP1BQ2/PP2B2q/R7 b - - 3 28",
+    solution="Rh1 Qh1 Qh1",
+    firstmove="c5",
+    difficulty=867)
+push!(rush_20_12_30, puzzle)
+
+puzzle = PuzzleFEN(
+    FEN="8/5pp1/2Qpk1b1/p1p1p3/4P3/1P1PN3/2P2PK1/4q3 w - - 0 35",
+    solution="Be4 de4 Qe4 f3 Qd5 Qd5 Kd5",
+    firstmove="Nd5",
+    difficulty=935)
+push!(rush_20_12_30, puzzle)
+
+puzzle = PuzzleFEN(
+    FEN="r4rk1/1b3ppp/8/pp2P1q1/2pp1n2/P1P1N3/1PBN2PP/R2Q1R1K w - - 0 1",
+    solution="Bg2 Ng2 Qg2",
+    firstmove="h3", # ??
+    difficulty=988)
+push!(rush_20_12_30, puzzle)
+
+puzzle = PuzzleFEN(
+    FEN="8/3N3p/6p1/5pk1/7P/5RP1/2r2PK1/4b3 b - h3 0 37",
+    solution="Nf6",
+    firstmove="Kg4",
+    difficulty=1018)
+push!(rush_20_12_30, puzzle)
+
+puzzle = PuzzleFEN(
+    FEN="5bk1/7R/b5p1/p5P1/4p3/3pK2R/1p5P/4B3 b - - 0 1",
+    solution="Rg7 Kg7 Bc3",
+    firstmove="Bg7",
+    difficulty=1056)
+push!(rush_20_12_30, puzzle)
+
+puzzle = PuzzleFEN(
+    FEN="r5r1/8/pp6/5p2/1b5p/3k1P2/PP3RPB/R5K1 w - - 0 32",
+    solution="Bc5 Rad1 Kc4 Kf1 Bf2",
+    firstmove="a3",
+    difficulty=1106)
+push!(rush_20_12_30, puzzle)
+
+puzzle = PuzzleFEN(
+    FEN="r1bq2k1/pp4pp/2n2r2/2b2p2/8/2NQ3P/PPP1NPP1/2KR1B1R b - - 0 12",
+    solution="Qc4 Be6 Qc5 Rd1 Nd1",
+    firstmove="Rd6",
+    difficulty=1189)
+push!(rush_20_12_30, puzzle)
+
+puzzle = PuzzleFEN(
+    FEN="r1bq1rk1/ppp2pp1/1n2p2p/4b1N1/2PnN3/3Q4/PPB2PPP/R1B1R1K1 b - - 3 16",
+    solution="Nf6 gf6 Qh7",
+    firstmove="Nc2",
+    difficulty=1243)
+push!(rush_20_12_30, puzzle)
+
+puzzle = PuzzleFEN(
+    FEN="8/p7/1p6/8/P1K4k/7P/8/8 b - - 1 47",
+    solution="Kb5 Kg4 Ka6 Kf5 Ka7 Ke6 Kb6 Kd7 Kb7",
+    firstmove="Kh3",
+    difficulty=1271)
+push!(rush_20_12_30, puzzle)
+
+puzzle = PuzzleFEN(
+    FEN="1k1r1Q2/1pp1Q3/p6p/8/6P1/P6P/KPP2p2/3q2bR b - - 0 30",
+    solution="Qf8 Ka7 Qf2 Bf2 Rd1",
+    firstmove="Rf8",
+    difficulty=1313)
+push!(rush_20_12_30, puzzle)
+
+puzzle = PuzzleFEN(
+    FEN="3rk1nr/p1q1ppbp/1pp3p1/2N5/2b1P3/3P1N1P/PP3PP1/R1BQ1RK1 w k - 0 1",
+    solution="Bb5 Qc2 bc5",
+    firstmove="Qa4",
+    difficulty=1379)
+push!(rush_20_12_30, puzzle)
+
+puzzle = PuzzleFEN(
+    FEN="4rk1r/8/6pp/p2qPp2/Pb1p1Q2/1P1P3P/6P1/1RB2RK1 w - - 3 28",
+    solution="Be1 Ba3 Bb4 Bb4 ab4",
+    firstmove="Re1",
+    difficulty=1442)
+push!(rush_20_12_30, puzzle)
+
+puzzle = PuzzleFEN(
+    FEN="4n3/p4p2/2N2k1p/1p6/2pp4/1P6/P1P2PPP/6K1 w - - 0 1",
+    solution="d3 cd3 c3 Nb5 c2",
+    firstmove="Na7",
+    difficulty=1483)
+push!(rush_20_12_30, puzzle)
+
+puzzle = PuzzleFEN(
+    FEN="r1bq2r1/ppp4Q/3pk3/4P1b1/5P2/8/PPP1n1PP/RN3R1K w - - 2 24",
+    solution="Ng3 hg3 Rh8 Qh8 Qh8",
+    firstmove="fg5",
+    difficulty=1540)
+push!(rush_20_12_30, puzzle)
+
+puzzle = PuzzleFEN(
+    FEN="r1bq1k1r/pp3pp1/3pn3/3N2b1/6Q1/1P4P1/PBP2P1P/R3R1K1 b - - 2 17",
+    solution="Re6 Be6 Qe6 Qe8 Qd6",
+    firstmove="f6",
+    difficulty=1589)
+push!(rush_20_12_30, puzzle)
+
+puzzle = PuzzleFEN(
+    FEN="1r2k2r/p1q2ppp/3bbn2/2p1N3/3p1B2/1PN5/P1PQ1PPP/R3R1K1 w k - 0 15",
+    solution="Be5 Nf6 gf6 Be5 fe5",
+    firstmove="Ne4",
+    difficulty=1633)
+push!(rush_20_12_30, puzzle)
